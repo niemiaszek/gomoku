7e77aade20bd41a3d8e1365196f4ba2e07b3e81d
Patryk Niemiec <1napiszdopatryka8@gmail.com>
2019-03-25 23:32:06 +0100

    Added semi-hard SI, some changes in code to make it work :)

--- Gomoku/stan.h
+++ Gomoku/stan.h
@@ -1,9 +1,9 @@
 #pragma once
 #ifndef STAN_H
 #define STAN_H
-char sprawdzStan(char **tab, int rozmiar, int pod_rzad);
-char sprawdzPion(char **tab, int rozmiar, int pod_rzad);
-char sprawdzPoziom(char **tab, int rozmiar, int pod_rzad);
-char sprawdzUkosy(char **tab, int rozmiar, int pod_rzad);
+char sprawdzStan(char **tab, int rozmiar, int pod_rzad, int &suma);
+char sprawdzPion(char **tab, int rozmiar, int pod_rzad, int &suma);
+char sprawdzPoziom(char **tab, int rozmiar, int pod_rzad, int &suma);
+char sprawdzUkosy(char **tab, int rozmiar, int pod_rzad, int &suma);
 char SprawdzRemis(char **tab, int rozmiar);
 #endif // !STAN_H
